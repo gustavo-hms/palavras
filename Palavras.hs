3324427783ff6e60d0f8b82dbb71a1dfe408d8e6
Gustavo Henrique Montesião de Sousa <gustavo@registro.br>
2013-01-02 16:57:01 -0200

    Métodos para criação de afixos e de regras

--- Palavras.hs
+++ Palavras.hs
@@ -1,33 +1,49 @@
 module Palavras where
 
-import qualified Data.Map as M
+import
 
 data Tipo = Prefixo | Sufixo deriving Show
 
 data Afixo = Afixo {
         tipo :: Tipo,
+        símbolo :: Char,
         permiteCruzamentos :: Bool,
         quantidade :: Int,
         regras :: [Regra]
-    }
+    } deriving Show
 
 data Regra = Regra {
         símbolosARemover :: Int,
-        afixo :: String,
+        texto :: String,
         condição :: String -> Bool
-    }
+    } deriving Show
 
-extrairTipo :: [String] -> (Maybe Tipo, [String])
-extrairTipo ("PFX":resto) = (Just Prefixo, resto)
-extrairTipo ("SFX":resto) = (Just Sufixo, resto)
-extrairTipo string        = (Nothing, string)
+criarAfixo :: [String] -> Maybe Afixo
+criarAfixo ("PFX":símb:cruzamentos:qtd:[]) =
+    Just $ Prefixo (head símb) (podeCruzar cruzamentos) (read qtd) []
+criarAfixo ("SFX":símb:cruzamentos:qtd:[]) =
+    Just $ Sufixo (head símb) (podeCruzar cruzamentos) (read qtd) []
+criarAfixo _ = Nothing
 
-especificação :: [String] -> (Char, Bool, Int)
-especificação (símbolo:cruzamentos:quantidade)
+podeCruzar :: String -> Bool
+podeCruzar "Y" = True
+podeCruzar _   = False
 
-criarAfixo :: [String] -> Afixo
-criarAfixo (regra:regras) =
-    let (tipo, resto) = extrairTipo regra
-        símbolo = head resto
-        cruzamentos = head $ tail resto
-        quantidade = last resto
+inserirRegra :: [String] -> Afixo -> Maybe Afixo
+inserirRegra _             Nothing = Nothing
+inserirRegra especificação afixo   = case regra of
+    Just r  -> Afixo t s c qtd (r:rs)
+    Nothing -> Nothing
+    where regra = criarRegra especificação afixo
+          t = tipo afixo
+          s = símbolo afixo
+          c = permiteCruzamentos afixo
+          qtd = quantidade afixo
+          rs = regras afixo
+
+criarRegra :: [String] -> Maybe Regra
+criarRegra ("PFX":símb:remover:prefixo:contexto:[]) =
+    Just $ Regra Prefixo (head símb) (read remover) prefixo (criarCondição contexto)
+criarRegra ("SFX":símb:remover:sufixo:contexto:[]) =
+    Just $ Regra Sufixo (head símb) (read remover) sufixo (criarCondição contexto)
+criarRegra _ = Nothing
