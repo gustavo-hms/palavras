7e1cbc60233bec853f0aa3ce050adb7dcc3fa124
Gustavo Henrique Montesião de Sousa <gustavo@cecm.usp.br>
2013-01-05 15:42:09 -0200

    Funções criarCondição, agrupar, condiçãoAPartirDeGrupos e predicado

--- Palavras.hs
+++ Palavras.hs
@@ -1,7 +1,5 @@
 module Palavras where
 
-import
-
 data Tipo = Prefixo | Sufixo deriving Show
 
 data Afixo = Afixo {
@@ -10,40 +8,67 @@ data Afixo = Afixo {
         permiteCruzamentos :: Bool,
         quantidade :: Int,
         regras :: [Regra]
-    } deriving Show
+    }
 
 data Regra = Regra {
         símbolosARemover :: Int,
         texto :: String,
         condição :: String -> Bool
-    } deriving Show
+    }
 
 criarAfixo :: [String] -> Maybe Afixo
 criarAfixo ("PFX":símb:cruzamentos:qtd:[]) =
-    Just $ Prefixo (head símb) (podeCruzar cruzamentos) (read qtd) []
+    Just $ Afixo Prefixo (head símb) (podeCruzar cruzamentos) (read qtd) []
 criarAfixo ("SFX":símb:cruzamentos:qtd:[]) =
-    Just $ Sufixo (head símb) (podeCruzar cruzamentos) (read qtd) []
+    Just $ Afixo Sufixo (head símb) (podeCruzar cruzamentos) (read qtd) []
 criarAfixo _ = Nothing
 
 podeCruzar :: String -> Bool
 podeCruzar "Y" = True
 podeCruzar _   = False
 
-inserirRegra :: [String] -> Afixo -> Maybe Afixo
-inserirRegra _             Nothing = Nothing
-inserirRegra especificação afixo   = case regra of
-    Just r  -> Afixo t s c qtd (r:rs)
-    Nothing -> Nothing
-    where regra = criarRegra especificação afixo
-          t = tipo afixo
+inserirRegra :: Regra -> Afixo -> Afixo
+inserirRegra r afixo = Afixo t s c qtd (r:rs)
+    where t = tipo afixo
           s = símbolo afixo
           c = permiteCruzamentos afixo
           qtd = quantidade afixo
           rs = regras afixo
 
-criarRegra :: [String] -> Maybe Regra
-criarRegra ("PFX":símb:remover:prefixo:contexto:[]) =
-    Just $ Regra Prefixo (head símb) (read remover) prefixo (criarCondição contexto)
-criarRegra ("SFX":símb:remover:sufixo:contexto:[]) =
-    Just $ Regra Sufixo (head símb) (read remover) sufixo (criarCondição contexto)
-criarRegra _ = Nothing
+--criarRegra :: [String] -> Maybe Regra
+--criarRegra ("PFX":símb:remover:prefixo:contexto:[]) =
+--    Just $ Regra Prefixo (head símb) (read remover) prefixo (criarCondição contexto)
+--criarRegra ("SFX":símb:remover:sufixo:contexto:[]) =
+--    Just $ Regra Sufixo (head símb) (read remover) sufixo (criarCondição contexto)
+--criarRegra _ = Nothing
+
+criarCondição :: String -> String -> Bool
+criarCondição símbolos = condiçãoAPartirDeGrupos $ snd (foldr agrupar (False, []) símbolos)
+
+-- Agrupa entradas entre colchetes.
+-- Ex: snd $ foldr agrupar (False, []) "[^a]xy[bcdef]" == ["^a", "x", 
+-- "y","bcdef"]
+agrupar :: Char -> (Bool, [String]) -> (Bool, [String])
+agrupar c (grupoAberto, acumulado)
+    | c == ']'  = (True, []:acumulado)
+    | c == '['  = (False, acumulado)
+    | otherwise = if grupoAberto
+                     then (True, (c:a):as)
+                     else (False, [c]:acumulado)
+                  where a = head acumulado
+                        as = tail acumulado
+
+condiçãoAPartirDeGrupos :: [String] -> String -> Bool
+condiçãoAPartirDeGrupos grupos palavra
+    | tamanhoDaPalavra < númeroDeGrupos = False
+    | otherwise                         = and $ zipWith ($) predicados finalDaPalavra 
+    where tamanhoDaPalavra = length palavra
+          númeroDeGrupos   = length grupos
+          finalDaPalavra   = drop (tamanhoDaPalavra - númeroDeGrupos) palavra
+          predicados       = map predicado grupos 
+
+predicado :: String -> Char -> Bool
+predicado "." letra                 = True
+predicado (c:[]) letra              = c == letra
+predicado ('[':'^':elementos) letra = letra `notElem` (init elementos)
+predicado ('[':elementos) letra     = letra `elem` (init elementos)
